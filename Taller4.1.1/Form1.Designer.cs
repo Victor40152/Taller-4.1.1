2359a6db7fbec7dd991c3159f26c5a18c62da9ed
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-10 20:24:08 -0500

    Taller 4 punto 1 tabla de multiplicar

---
+++ Taller4.1.1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Taller4._1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btningreso = new System.Windows.Forms.Button();
+            this.dgdatos = new System.Windows.Forms.DataGridView();
+            this.Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Consecutivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Resultado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgdatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el Número:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(363, 37);
+            this.txtnombre.Multiline = true;
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(106, 26);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // btningreso
+            // 
+            this.btningreso.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btningreso.Location = new System.Drawing.Point(510, 37);
+            this.btningreso.Name = "btningreso";
+            this.btningreso.Size = new System.Drawing.Size(123, 31);
+            this.btningreso.TabIndex = 2;
+            this.btningreso.Text = "Ingresar";
+            this.btningreso.UseVisualStyleBackColor = true;
+            this.btningreso.Click += new System.EventHandler(this.btningreso_Click);
+            // 
+            // dgdatos
+            // 
+            this.dgdatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgdatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Valor,
+            this.Consecutivo,
+            this.Resultado});
+            this.dgdatos.Location = new System.Drawing.Point(106, 175);
+            this.dgdatos.Name = "dgdatos";
+            this.dgdatos.Size = new System.Drawing.Size(621, 194);
+            this.dgdatos.TabIndex = 3;
+            this.dgdatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgdatos_CellContentClick);
+            // 
+            // Valor
+            // 
+            this.Valor.HeaderText = "Valor";
+            this.Valor.Name = "Valor";
+            this.Valor.ReadOnly = true;
+            // 
+            // Consecutivo
+            // 
+            this.Consecutivo.HeaderText = "Consecutivo";
+            this.Consecutivo.Name = "Consecutivo";
+            this.Consecutivo.ReadOnly = true;
+            // 
+            // Resultado
+            // 
+            this.Resultado.HeaderText = "Resultado";
+            this.Resultado.Name = "Resultado";
+            this.Resultado.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(510, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Borrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgdatos);
+            this.Controls.Add(this.btningreso);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgdatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btningreso;
+        private System.Windows.Forms.DataGridView dgdatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Consecutivo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resultado;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Taller4.1.1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller4._1._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dgdatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btningreso_Click(object sender, EventArgs e)
+        {
+            int numero, total, contador;
+            numero = int.Parse(txtnombre.Text);
+            contador = 0;
+            int limite = 10;
+           for (int i = 1; i <=limite;i++)
+            {
+
+                contador = contador + 1;
+
+               total = numero * contador;
+                dgdatos.Rows.Add(numero, contador.ToString(), total.ToString());
+
+         
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgdatos.Rows.Clear();
+            txtnombre.Text = "";
+            txtnombre.Focus();
+        }
+    }
+}
